439eaa6addf825a7084e0afeb0e896425782a6d6
Jaan Janesmae <git@janesmae.com>
2018-09-27 20:05:32 +0300

    Add app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 var http = require('http');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var phjs = require('phantomjs').path;
 
 const PORT=8080;
 
 function handleRequest(request, response){
 
-	console.log('test');
+	var request_data = '';
+	url = request.url;
+	real_url = url.substr(1);
+
+	console.log('url: ' + real_url);
+
+	ls = spawn(phjs, ['src/screenshot.js',real_url]);
+
+	ls.stdout.on('data', function (data) {
+		console.log('stdout: ' + data);
+		request_data = request_data + data;
+	});
+
+	ls.stderr.on('data', function (data) {
+		console.log('stderr: ' + data);
+	});
+
+	ls.on('close', function (code) {
+		console.log('child process exited with code ' + code);
+		response.end(request_data);
+	});
 
 }
 
